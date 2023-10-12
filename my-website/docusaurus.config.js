ff3a76efb832da115f540f77279b3a3db6db60a3
fengkan <sunnyviewtech@gmail.com>
2023-10-12 14:02:11 +0800

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://sunnyviewtech.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sunnyviewtech', // Usually your GitHub org/user name.
-  projectName: 'sunnyviewtech.github.io', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
